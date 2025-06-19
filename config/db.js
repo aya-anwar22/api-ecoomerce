9d42888ec179b5b0f10ab3d0f4274a076f720ae2
aya-anwar22 <aya786930@gmail.com>
2025-06-19 12:07:29 +0300

    back end

--- config/db.js
+++ config/db.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(`❌ MongoDB Connection Error: ${err.message}`);
-    process.exit(1); // Exit process with failure
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1); 
   }
 };
 
